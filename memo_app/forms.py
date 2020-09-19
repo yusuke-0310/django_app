e6d98dd249af4ef75b36cfb3d7d3a3921107dd92
Yusuke <yuui0ig@gmail.com>
2020-09-19 14:23:27 +0900

    second-commit

--- memo_app/forms.py
+++ memo_app/forms.py
@@ -15,6 +15,17 @@ CHOICE_FIELD_RECODE_NUMBERS = (
     ('30', '30件'),
 )
 
+CHOICE_FIELD_ORDER_OPTION = (
+    ('new', '新着順'),
+    ('old', '投稿順')
+)
+
+class Set_order_option(forms.Form):
+    order_option = forms.ChoiceField(
+        widget=forms.Select(attrs={'onchange': 'submit(this.form)'}),
+        choices=CHOICE_FIELD_ORDER_OPTION
+    )
+
 class RecordNumberForm(forms.Form):
     record_number = forms.ChoiceField(
         widget=forms.Select(attrs={'onchange': 'submit(this.form)'}),
--- memo_app/views.py
+++ memo_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import PostForm, RecordNumberForm
+from .forms import PostForm, RecordNumberForm, Set_order_option
 from .models import *
 from django.core.paginator import Paginator
 
@@ -12,12 +12,25 @@ def index(request, now_page=1):
     record_number_form = RecordNumberForm()
     record_number_form.initial = {'record_number': str(record_number)}
 
-    memos = Memo.objects.all().order_by('update_datetime').reverse()
+    if 'order_option' in request.session:
+        order_option = request.session['order_option']
+    else:
+        order_option = 'new'
+
+    order_option_form = Set_order_option()
+    order_option_form.initial = {'order_option': str(order_option)}
+
+    if order_option == 'new':
+        memos = Memo.objects.all().order_by('update_datetime').reverse()
+    else:
+        memos = Memo.objects.all().order_by('update_datetime')
+
     page = Paginator(memos, record_number)
     params = {
         'page': page.get_page(now_page),
         'form': PostForm(),
         'record_number_form': record_number_form,
+        'order_option_form': order_option_form
     }
     return render(request, 'index.html', params)
 
@@ -32,4 +45,8 @@ def post(request):
 
 def set_record_number(request):
     request.session['record_number'] = request.POST['record_number']
+    return redirect(to='/')
+
+def set_order_option(request):
+    request.session['order_option'] = request.POST['order_option']
     return redirect(to='/')
